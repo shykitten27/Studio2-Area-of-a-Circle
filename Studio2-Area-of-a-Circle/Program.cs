b300a71972e6c0f44cee09565f06958bc0654457
D Seidel <67442972+shykitten27@users.noreply.github.com>
2021-07-14 18:38:30 -0500

    Add project files.

---
+++ Studio2-Area-of-a-Circle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Studio2_Area_of_a_Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
