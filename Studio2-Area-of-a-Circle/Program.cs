8b523c3671244268597b568c699eb0a5f3eafdae
D Seidel <67442972+shykitten27@users.noreply.github.com>
2021-07-14 18:55:15 -0500

    revert to math.pi add circumference and diameter

--- Studio2-Area-of-a-Circle/Program.cs
+++ Studio2-Area-of-a-Circle/Program.cs
@@ -7,15 +7,20 @@ namespace Studio2_Area_of_a_Circle
         static void Main(string[] args)
         {
             string input;
-            double pi = 3.14;
             double radius;
             double area;
+            double circumference;
+            double diameter;
 
             Console.WriteLine("Enter a radius:");
             input = Console.ReadLine();
             radius = double.Parse(input);
-            area = pi * Math.Pow(radius, 2.0);
+            area = Math.PI * Math.Pow(radius, 2.0);
+            circumference = 2 * Math.PI * radius;
+            diameter = 2 * radius;
             Console.WriteLine("The area of a circle of radius " + radius + " is: " + area);
+            Console.WriteLine("The circumference of a circle of radius " + radius + " is: " + circumference);
+            Console.WriteLine("The diameter of a circle of radius " + radius + " is: " + diameter);
         }
     }
 }
