d0288595c3c88821efd59a6888ea7dfde9fa038d
D Seidel <67442972+shykitten27@users.noreply.github.com>
2021-07-14 18:50:38 -0500

    declare pi vs using math.pi

--- Studio2-Area-of-a-Circle/Program.cs
+++ Studio2-Area-of-a-Circle/Program.cs
@@ -7,13 +7,14 @@ namespace Studio2_Area_of_a_Circle
         static void Main(string[] args)
         {
             string input;
+            double pi = 3.14;
             double radius;
             double area;
 
             Console.WriteLine("Enter a radius:");
             input = Console.ReadLine();
             radius = double.Parse(input);
-            area = Math.PI * Math.Pow(radius, 2.0);
+            area = pi * Math.Pow(radius, 2.0);
             Console.WriteLine("The area of a circle of radius " + radius + " is: " + area);
         }
     }
