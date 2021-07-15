d72a72c56923ed503d9243c22ec5b9633b8dca32
D Seidel <67442972+shykitten27@users.noreply.github.com>
2021-07-15 13:28:51 -0500

    working on bonus

---
+++ Studio2-Area-of-a-Circle/Circle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Studio2_Area_of_a_Circle
+{
+    public class Circle
+    {
+        public static double GetArea(double radius)
+        {
+            double area = Math.Round(Math.PI * Math.Pow(radius, 2.0), 2);
+            return area;
+        }
+
+        public static double GetCircumference(double radius)
+        {
+            double circumference = Math.Round((2 * Math.PI * radius), 2);
+            return circumference;
+        }
+
+        public static double GetDiameter(double radius)
+        {
+            double diameter = Math.Round((2 * radius), 2); ;
+            return diameter;
+        }
+
+    }
+}
--- Studio2-Area-of-a-Circle/Program.cs
+++ Studio2-Area-of-a-Circle/Program.cs
@@ -7,20 +7,39 @@ namespace Studio2_Area_of_a_Circle
         static void Main(string[] args)
         {
             string input;
-            double radius;
-            double area;
-            double circumference;
-            double diameter;
+            //double radius;
+            double mpg;
+            double amtOfGas;
 
             Console.WriteLine("Enter a radius:");
             input = Console.ReadLine();
-            radius = double.Parse(input);
-            area = Math.PI * Math.Pow(radius, 2.0);
-            circumference = 2 * Math.PI * radius;
-            diameter = 2 * radius;
-            Console.WriteLine("The area of a circle of radius " + radius + " is: " + area);
-            Console.WriteLine("The circumference of a circle of radius " + radius + " is: " + circumference);
-            Console.WriteLine("The diameter of a circle of radius " + radius + " is: " + diameter);
+            double result;
+            //radius = double.Parse(input);
+
+            while (input == "" || !double.TryParse(input, out result) || double.Parse(input) < 0)
+            {
+                Console.WriteLine("Please enter a valid radius");
+                input = Console.ReadLine();
+
+            }
+
+            double radius = double.Parse(input);
+            Console.WriteLine("Enter miles per gallon of your car:");
+            input = Console.ReadLine();
+            mpg = double.Parse(input);
+
+            amtOfGas = Math.Round((Circle.GetCircumference(radius)/mpg),2);
+
+            Console.WriteLine("The area of a circle of radius " + radius + " is: " + Circle.GetArea(radius));
+            Console.WriteLine("The circumference of a circle of radius " + radius + " is: " + Circle.GetCircumference(radius));
+            Console.WriteLine("The diameter of a circle of radius " + radius + " is: " + Circle.GetDiameter(radius));
+            Console.WriteLine("Amount of gas consumed to drive " + Circle.GetCircumference(radius) + " is: " + amtOfGas + ".");
+
+        }
+
+        private static bool IsNaN(double radius)
+        {
+            throw new NotImplementedException();
         }
     }
 }
